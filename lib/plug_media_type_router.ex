1b2b29c5237fefaa29fa182ec4b58fde1ce67a06
Daniel Grieve <git@cazrin.net>
2016-01-04 23:17:23 +0000

    Implement first pass at plug
    Most of this was extracted from another project, so no small commits
    here.

--- lib/plug_media_type_router.ex
+++ lib/plug_media_type_router.ex
@@ -1,2 +1,60 @@
 defmodule PlugMediaTypeRouter do
+  import Plug.Conn
+  alias Plug.Conn.Status
+
+  def init(opts), do: opts
+
+  def call(conn, opts) do
+    routers = Keyword.fetch!(opts, :routers)
+    version = accept_header_version(conn, opts)
+
+    conn
+    |> pass_to_router(Dict.get(routers, version), opts)
+  end
+
+  defp accept_header_version(conn, opts) do
+    default_version = Keyword.fetch!(opts, :default_version)
+    name = Keyword.fetch!(opts, :name)
+
+    conn
+    |> get_req_header("accept")
+    |> List.first()
+    |> extract_version(name, default_version)
+  end
+
+  defp extract_version(nil, _, default_version), do: default_version
+  defp extract_version(header, name, default_version) do
+    pattern = ~r/application\/vnd\.#{name}\.(v\d)*/
+
+    case Regex.scan(pattern, header) do
+      [[_match, version]] -> version
+      _ -> default_version
+    end
+  end
+
+  defp error_message(version, opts) do
+    versions =
+      opts
+      |> Keyword.fetch!(:routers)
+      |> Dict.keys()
+      |> Enum.join(", ")
+
+    """
+    API version not supported: #{version}
+
+    Currently supported API versions: #{versions}
+    """
+  end
+
+  defp pass_to_router(conn, nil, opts) do
+    body = conn |> accept_header_version(opts) |> error_message(opts)
+
+    conn
+    |> send_resp(Status.code(:bad_request), body)
+    |> halt()
+  end
+
+  defp pass_to_router(conn, router, _opts) do
+    router.call(conn, router.init([]))
+  end
 end
--- test/plug_media_type_router_test.exs
+++ test/plug_media_type_router_test.exs
@@ -1,4 +1,72 @@
 defmodule PlugMediaTypeRouterTest do
   use ExUnit.Case
-  doctest PlugMediaTypeRouter
+  use Plug.Test
+
+  alias PlugMediaTypeRouter, as: TestSubject
+
+  defmodule MyApp.V1.Router do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    get "/", do: conn |> resp(200, "v1")
+  end
+
+  defmodule MyApp.V2.Router do
+    use Plug.Router
+
+    plug :match
+    plug :dispatch
+
+    get "/", do: conn |> resp(200, "v2")
+  end
+
+  @options default_version: "v2",
+           name: "my_app",
+           routers: %{
+             "v1" => MyApp.V1.Router,
+             "v2" => MyApp.V2.Router
+           }
+
+  test "routing to the default version" do
+    conn =
+      conn(:get, "/")
+      |> TestSubject.call(@options)
+
+    assert conn.resp_body == "v2"
+  end
+
+  test "routing with other accept headers" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("accept", "text/html")
+      |> TestSubject.call(@options)
+
+    assert conn.resp_body == "v2"
+  end
+
+  test "routing to other versions" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("accept", "application/vnd.my_app.v1")
+      |> TestSubject.call(@options)
+
+    assert conn.resp_body == "v1"
+  end
+
+  test "requesting a non-existent version" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("accept", "application/vnd.my_app.v3")
+      |> TestSubject.call(@options)
+
+    assert conn.halted
+    assert conn.resp_body == """
+    API version not supported: v3
+
+    Currently supported API versions: v1, v2
+    """
+    assert conn.status == 400
+  end
 end
