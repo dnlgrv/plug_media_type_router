cc98be02ca6faea8bde71b19d1e6e2a986be191b
Daniel Grieve <git@cazrin.net>
2016-01-04 23:42:36 +0000

    Generate documentation for hex

--- mix.exs
+++ mix.exs
@@ -3,11 +3,15 @@ defmodule PlugMediaTypeRouter.Mixfile do
 
   def project do
     [app: :plug_media_type_router,
+     name: "PlugMediaTypeRouter",
+     source_url: "https://github.com/cazrin/plug_media_type_router",
+     homepage_url: "https://github.com/cazrin/plug_media_type_router",
      version: "0.0.1",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     docs: docs]
   end
 
   def application do
@@ -15,6 +19,13 @@ defmodule PlugMediaTypeRouter.Mixfile do
   end
 
   defp deps do
-    [{:plug, "~> 1.0.0"}]
+    [{:plug, "~> 1.0.0"},
+
+     {:earmark, "~> 0.1.19", only: :docs},
+     {:ex_doc, "~> 0.11", only: :docs}]
+  end
+
+  defp docs do
+    [extras: ["README.md"], main: "README"]
   end
 end
