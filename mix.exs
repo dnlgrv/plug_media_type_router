30144c4977af8f0718b22e0fa463a18447eb47a0
Daniel Grieve <daniel@danielgrieve.uk>
2017-05-26 12:03:11 +0100

    Change environment for ex_doc

--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule PlugMediaTypeRouter.Mixfile do
   defp deps do
     [{:plug, ">= 1.0.0"},
 
-     {:earmark, "~> 0.1.19", only: :docs},
-     {:ex_doc, "~> 0.11", only: :docs}]
+     {:earmark, "~> 0.1.19", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 
   defp docs do
